9da3f1e92b6a5e7516d12f780784b7e2a5035e6e
Dinhsiduyet <dinhsyduyet@outlook.com>
2020-11-27 20:10:50 +0700

    add loading

--- main.js
+++ main.js
@@ -91,3 +91,21 @@ function hide(){
 
 
 
+function onReady(callback) {
+    var intervalId = window.setInterval(function() {
+      if (document.getElementsByTagName('body')[0] !== undefined) {
+        window.clearInterval(intervalId);
+        callback.call(this);
+      }
+    }, 6000);
+  }
+  
+  function setVisible(selector, visible) {
+    document.querySelector(selector).style.display = visible ? 'block' : 'none';
+  }
+  
+  onReady(function() {
+    setVisible('.wrapper', true);
+    setVisible('.container-loading', false);
+  });
+
